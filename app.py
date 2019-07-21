22400e85de29360ebdf2b215f2dec0048932c614
Mikaela Sheldt <msheldt@gmail.com>
2019-07-21 13:36:46 -0400

    Howdy Ya'll

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# always refer to yourself
+app = Flask(__name__)
+
+# ROUTES
+@app.route('/')
+def index():
+    return "Howdy Ya'll"
+
+# debug=True -> any time I make changes we want flask ot auto-restart
+# host='0.0.0.0' -> listen on all addresses that can get here
+app.run(debug=True, port=8000, host='0.0.0.0')
