3a6c1176bc58b48290e65faf8c09daa60889b7f7
Mikaela Sheldt <msheldt@gmail.com>
2019-07-21 16:20:05 -0400

    send pure html as a repsonse

--- app.py
+++ app.py
@@ -25,14 +25,28 @@ def clean_view(name):
 def int_input(number):
     """specifiy a type in the url"""
     return "I am {}, and I have always been an integer. Two of me makes {}.".format(number, number*2)
+
 @app.route('/convert/<number>')
 def int_maker(number):
     """
-    Remember that input is always a string.
+    Remember that input is always a string. And you must always return a string!
     """
     integer = int(number)
     return "My string is {}, but if you change me to an int, you can add one and I become {}.".format(number, integer+1)
 
+@app.route('/oldschool/html')
+def render_html():
+    return """
+    <!doctype html>
+    <html>
+    <head><title>This is the hard way . . .</title></head>
+    <body>
+        <h1>
+            This is html rendered old-school without templates.
+        </h1>
+    </body>
+    </html>
+    """
 
 # debug=True -> any time I make changes we want flask ot auto-restart
 # host='0.0.0.0' -> listen on all addresses that can get here
